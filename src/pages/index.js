a34a4fbf86764af41e329ae1e5512a3ad803cfa7
taka2 <taka314@live.com>
2018-12-12 15:32:51 -0800

    next articles button

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,8 @@ const IndexPage = () => (
   <div>
     <h1>My goal is to copy my huzzbin's blog</h1>
     <p>Fingers crossed🤞🏻 Let's get to work!!👩🏻‍💻</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to the next articles</Link>
   </div>
 )
 
 export default IndexPage
-
