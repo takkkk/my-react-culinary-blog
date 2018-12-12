9c75de58cb8adf976cfbcf6b21fc9eb6aa64414c
taka2 <taka314@live.com>
2018-12-12 15:22:54 -0800

    second commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>My goal is to copy my huzzbin's blog</h1>
+    <p>Fingers crossed🤞🏻 Let's get to work!!👩🏻‍💻</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
+
